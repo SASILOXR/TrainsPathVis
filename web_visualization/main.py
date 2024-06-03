5d7f3997e787d0fb0813cc5a8ce6db3fd9411b2c
SASILOXR <ppjtg@qq.com>
2024-06-03 19:27:03 +0800

    fix wrong

--- web_visualization/main.py
+++ web_visualization/main.py
@@ -6,9 +6,10 @@ import math
 import copy
 
 app = Flask(__name__)
-
+#Dijkstra算法
 # 利用起始位置和邻接矩阵计算最短路
 def shortestPath(start_s, mgraph, sortby):
+    #start relaTable mode
     stations = mgraph.index.tolist()
     start = 0
     for i in range(len(stations)):
@@ -17,7 +18,7 @@ def shortestPath(start_s, mgraph, sortby):
     # 存储已知最小长度的节点编号 即是顺序
     passed = [start]
     nopass = [x for x in range(len(stations)) if x != start]
-
+    #get 权重， 0为时间， 1为距离
     dis = [item[sortby] for item in mgraph[start_s]]
 
     # 创建字典 为直接与start节点相邻的节点初始化路径
@@ -35,42 +36,27 @@ def shortestPath(start_s, mgraph, sortby):
         passed.append(idx)
 
         for i in nopass:
-            if dis[idx] + mgraph[stations[idx]][stations[i]][0] < dis[i]: 
-                dis[i] = dis[idx] + mgraph[stations[idx]][stations[i]][0]
+            if dis[idx] + mgraph[stations[idx]][stations[i]][sortby] < dis[i]: 
+                dis[i] = dis[idx] + mgraph[stations[idx]][stations[i]][sortby]
                 dict_[stations[i]] = dict_[stations[idx]] + [stations[idx]]
     return dis,dict_
 
 # 前端由此路由跳转到对应网页
-@app.route("/StationVis", methods=['GET', 'POST'])
-def StationVis():
-    return app.send_static_file("StationVis.html")
-
-@app.route("/TrainVis", methods=['GET', 'POST'])
-def TrainVis():
-    return app.send_static_file("TrainVis.html")
-
-@app.route("/AccessVis", methods=['GET', 'POST'])
-def AccessVis():
-    return app.send_static_file("AccessVis.html")
-
-@app.route("/ChartVis", methods=['GET', 'POST'])
-def ChartVis():
-    return app.send_static_file("ChartVis.html")
-
 @app.route("/PathVis", methods=['GET', 'POST'])
 def PathVis():
     return app.send_static_file("PathVis.html")
 
 # 前端由此路由获取最短路信息的json串
-@app.route("/GetPath", methods=['GET', 'POST'])
+@app.route("/GetPath", methods=['GET', 'POST'])#api pointend
 def GetPath():
     start = request.form.get('start')
     end = request.form.get('end')
     sortby = request.form.get('sortby')
     #print(start + end)
-    df= pd.read_json("static/data/min_adjacency_table.json")
+    df= pd.read_json("static/data/min_adjacency_table.json")#read relation table
     dis = []
     dict_ = {}
+    #handle best
     if sortby == "时间优先":
         dis,dict_ = shortestPath(start, df, 0)
     elif sortby == "距离优先":
@@ -80,7 +66,7 @@ def GetPath():
     res = {}
     for i in range(len(stations)):
         if stations[i] == end:
-            res['path'] = dict_[stations[i]]+[stations[i]]
+            res['path'] = dict_[stations[i]] +  [stations[i]]
             if sortby == "时间优先":
                 res['time'] = str(dis[i])+" mins"
             elif sortby == "距离优先":
